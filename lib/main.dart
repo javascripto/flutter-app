79258c2dd2549bde622374e08de9a05995bc48d8
Yuri Almeida <yurialves2@gmail.com>
2021-10-01 20:21:21 -0300

    chore: add image

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,20 @@ class BytebankApp extends StatelessWidget {
   }
 }
 
-
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+        ],
       ),
     );
   }
