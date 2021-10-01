25c6e3773574ec9312de1546a7a91db7d96b0ea3
Yuri Almeida <yurialves2@gmail.com>
2021-10-01 20:38:49 -0300

    chore: init contacts_list widget

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('John Doe', style: TextStyle(fontSize: 24)),
+              subtitle: Text('1000', style: TextStyle(fontSize: 16)),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
