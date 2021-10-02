9b27cda62cd3efda55f52b8ab219b9043ff49950
Yuri Almeida <yurialves2@gmail.com>
2021-10-01 23:09:54 -0300

    chore: created contact db functions

---
+++ lib/database/app_database.dart
@@ -0,0 +1,66 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter_app/models/contact.dart';
+
+Future<Database> createDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  return openDatabase(path, version: 1, onCreate: (db, version) {
+    db.execute('''
+      CREATE TABLE contacts (
+        int id PRIMARY KEY,
+        name TEXT,
+        account_number INTEGER
+      )
+    ''');
+  });
+}
+
+Future<int> save(Contact contact) async {
+  Database db = await createDatabase();
+  int id = await db.insert('contacts', {
+    'name': contact.name,
+    'account_number': contact.accountNumber,
+  });
+  return id;
+}
+
+Future<List<Contact>> findAll() async {
+  Database db = await createDatabase();
+  List<Map> contactMapsList = await db.query('contacts');
+  List<Contact> contactList = contactMapsList
+      .map(
+        (contactMap) => Contact(
+          id: contactMap['id'],
+          name: contactMap['name'],
+          accountNumber: contactMap['account_number'],
+        ),
+      )
+      .toList();
+  return contactList;
+}
+
+Future<Contact?> find(int id) async {
+  Database db = await createDatabase();
+  List<Map> contactMapsList = await db.query(
+    'contacts',
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+  if (contactMapsList.isEmpty) return null;
+  return Contact(
+    id: contactMapsList.first['id'],
+    name: contactMapsList.first['name'],
+    accountNumber: contactMapsList.first['account_number'],
+  );
+}
+
+Future<int> delete(int id) async {
+  Database db = await createDatabase();
+  int affectedRows = await db.delete(
+    'contacts',
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+  return affectedRows;
+}
