27aaeda5b555995a4200d77ab29406b048609f32
Yuri Almeida <yurialves2@gmail.com>
2021-10-02 00:43:26 -0300

    refactor: added serializable methods

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -5,6 +5,17 @@ class Contact {
 
   Contact({this.id, this.name = '', this.accountNumber});
 
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'accountNumber': accountNumber,
+      };
+
   @override
   String toString() {
     return 'Contact{id: $id, name: $name, accountNumber: $accountNumber}';
