9fcec351f90c0f8249a30d8a9468cc6426a0f452
Yuri Almeida <yurialves2@gmail.com>
2021-10-01 21:33:01 -0300

    chore: create contact with form values

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/contact.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +27,7 @@ class ContactForm extends StatelessWidget {
                 labelText: 'Full name',
               ),
               style: TextStyle(fontSize: 24),
+              controller: _nameController,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8),
@@ -25,6 +37,7 @@ class ContactForm extends StatelessWidget {
                 ),
                 style: TextStyle(fontSize: 24),
                 keyboardType: TextInputType.number,
+                controller: _accountNumberController,
               ),
             ),
             Padding(
@@ -32,7 +45,14 @@ class ContactForm extends StatelessWidget {
               child: SizedBox(
                 width: double.maxFinite,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int? accountNumber =
+                        int.tryParse(_accountNumberController.text);
+                    final Contact contact =
+                        Contact(name: name, accountNumber: accountNumber);
+                    Navigator.pop(context, contact);
+                  },
                   child: Text('Create'),
                 ),
               ),
