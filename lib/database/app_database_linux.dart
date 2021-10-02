1d4c7df4b2720ea0f200bd55d660fd793a1425f2
Yuri Almeida <yurialves2@gmail.com>
2021-10-02 00:43:40 -0300

    chore: created storage for linux

---
+++ lib/database/app_database_linux.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:flutter_app/models/contact.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+int _lastIndex = 0;
+
+Future<int> save(Contact contact) async {
+  final storage = await SharedPreferences.getInstance();
+  List<String> contacts = storage.getStringList('contacts') ?? [];
+
+  if (contacts.isNotEmpty) {
+    Contact contact = Contact.fromJson(jsonDecode(contacts.last));
+    _lastIndex = contact.id ?? _lastIndex;
+  }
+  contact.id = contact.id ?? ++_lastIndex;
+  contacts.add(jsonEncode(contact.toJson()));
+  storage.setStringList('contacts', contacts);
+  return contact.id!;
+}
+
+Future<List<Contact>> findAll() async {
+  final storage = await SharedPreferences.getInstance();
+  List<String> contactsString = storage.getStringList('contacts') ?? [];
+  List<Contact> contacts = contactsString.map((contactString) {
+    return Contact.fromJson(jsonDecode(contactString));
+  }).toList();
+  return contacts;
+}
+
+Future<Contact?> find(int id) async {
+  return findAll().then(
+    (contacts) => contacts.firstWhere((contact) => contact.id == id),
+  );
+}
+
+Future<int> delete(int id) async {
+  final storage = await SharedPreferences.getInstance();
+  List<Contact> contacts = await findAll();
+  int affectedRows = 0;
+  contacts.removeWhere((contact) {
+    if (contact.id == id) {
+      affectedRows++;
+      return true;
+    }
+    return false;
+  });
+  List<String> newContactsString = contacts.map((contact) {
+    return json.encode(contact.toJson());
+  }).toList();
+  storage.setStringList('contacts', newContactsString);
+  return affectedRows;
+}
