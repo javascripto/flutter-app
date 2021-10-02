217124b6b86c1b7e4ac2a4a79f957c3f852567cf
Yuri Almeida <yurialves2@gmail.com>
2021-10-02 01:28:33 -0300

    refactor: add ListView builder with fake contacts

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -3,22 +3,24 @@ import 'package:flutter_app/models/contact.dart';
 import 'package:flutter_app/screens/contact_form.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = [
+    Contact(name: 'John Doe', accountNumber: 12345),
+    Contact(name: 'Jane Doe', accountNumber: 67890),
+    Contact(name: 'Anne Parker', accountNumber: 37283),
+    Contact(name: 'Joe Smith', accountNumber: 54321),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('John Doe', style: TextStyle(fontSize: 24)),
-              subtitle: Text('1000', style: TextStyle(fontSize: 16)),
-            ),
-          ),
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return _ContactItem(contact: contacts[index]);
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final Contact newContact = await Navigator.of(context).push(
@@ -27,6 +29,23 @@ class ContactsList extends StatelessWidget {
           debugPrint(newContact.toString());
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem({required this.contact});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text('${contact.name}', style: TextStyle(fontSize: 24)),
+        subtitle:
+            Text('${contact.accountNumber}', style: TextStyle(fontSize: 16)),
       ),
     );
   }
