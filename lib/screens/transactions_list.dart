c0a40e3a1fe5e1c3130c0a94dacfae5e9ff8f929
Yuri Almeida <yurialves2@gmail.com>
2021-10-12 18:30:10 -0300

    using futurebuilder do make request

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,44 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/contact.dart';
+
+import 'package:flutter_app/http/webclient.dart';
 import 'package:flutter_app/models/transaction.dart';
+import 'package:flutter_app/components/centralized_loading.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
-
   @override
   Widget build(BuildContext context) {
-    transactions.add(Transaction(
-      value: 100.00,
-      contact: Contact(name: 'Alex', accountNumber: 1000),
-    ));
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+      body: FutureBuilder<List<Transaction>>(
+        future: findAll(),
+        builder: (context, snapshot) {
+          final transactions = snapshot.data;
+          if (transactions == null) return CentralizedLoading();
+          return ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (context, index) {
+              final Transaction transaction = transactions[index];
+              return Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text(
+                    transaction.value.toString(),
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    transaction.contact.accountNumber.toString(),
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
                 ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
+              );
+            },
           );
         },
-        itemCount: transactions.length,
       ),
     );
   }
