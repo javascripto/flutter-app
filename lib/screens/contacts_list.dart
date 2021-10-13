39eba15221addec599f4f165384f441cb516d856
Yuri Almeida <yurialves2@gmail.com>
2021-10-13 01:00:51 -0300

    chore: save transaction wid webclient

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/models/contact.dart';
 import 'package:flutter_app/screens/contact_form.dart';
 import 'package:flutter_app/database/dao/contact_dao.dart';
 import 'package:flutter_app/components/centered_loading.dart';
+import 'package:flutter_app/screens/transaction_form.dart';
 
 class ContactsList extends StatefulWidget {
   @override
@@ -26,7 +27,17 @@ class _ContactsListState extends State<ContactsList> {
           return ListView.builder(
             itemCount: contacts.length,
             itemBuilder: (context, index) {
-              return _ContactItem(contact: contacts[index]);
+              final contact = contacts[index];
+              return _ContactItem(
+                contact: contact,
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) {
+                      return TransactionForm(contact: contact);
+                    }),
+                  );
+                },
+              );
             },
           );
         },
@@ -46,13 +57,15 @@ class _ContactsListState extends State<ContactsList> {
 
 class _ContactItem extends StatelessWidget {
   final Contact contact;
+  final Function() onTap;
 
-  _ContactItem({required this.contact});
+  _ContactItem({required this.contact, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: onTap,
         title: Text('${contact.name}', style: TextStyle(fontSize: 24)),
         subtitle: Text(
           '${contact.accountNumber}',
