420ff4ed933a310d04726b80ce4c51dc162dfbae
Yuri Almeida <yurialves2@gmail.com>
2021-10-13 01:00:35 -0300

    chore: added transactions form

---
+++ lib/screens/transaction_form.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/http/webclient.dart';
+
+import '../models/contact.dart';
+import '../models/transaction.dart';
+
+class TransactionForm extends StatefulWidget {
+  final Contact contact;
+
+  TransactionForm({required this.contact});
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final TextEditingController _valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New transaction'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                widget.contact.name,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  widget.contact.accountNumber.toString(),
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _valueController,
+                  style: TextStyle(fontSize: 24.0),
+                  decoration: InputDecoration(labelText: 'Value'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    child: Text('Transfer'),
+                    onPressed: () async {
+                      final double value =
+                          double.tryParse(_valueController.text)!;
+                      final transactionCreated = Transaction(
+                        value: value,
+                        contact: widget.contact,
+                      );
+                      try {
+                        await save(transactionCreated);
+                        Navigator.pop(context, transactionCreated);
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
