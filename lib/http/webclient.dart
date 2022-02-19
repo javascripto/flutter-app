235390b8fa9a51805d59100cd0dacf21e10472f7
Yuri <yurialves2@gmail.com>
2022-02-19 19:51:21 -0300

    chore: add default request timeout

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,7 @@
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:flutter_app/http/interceptors/logging_interceptor.dart';
 
-final client = InterceptedClient.build(interceptors: [
-  LoggingInterceptor(),
-]);
+final client = InterceptedClient.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 5),
+);
